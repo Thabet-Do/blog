d7a646c415fd38f99220415b004e996d0c43a53a
sabeat <sabeat2233@gmail.com>
2019-04-22 10:03:08 +0300

    add laradock for create docker img
    - dockerfile is inside 'laradock' directory
    -Main Commands:
        $ cd laradock
        $ docker-compose up -d nginx mysql phpmyadmin redis workspace
        $ docker-compose exec workspace bash

--- config/database.php
+++ config/database.php
@@ -48,7 +48,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'users-project'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', '123'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
